573dc6f3b2befea5c21bd158b38414f35a121165
nishanthann <nizhanth23@gmail@gmail.com>
2025-07-30 17:18:06 +0530

    feat: implement habit tracking with completion and deletion

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,31 +1,57 @@
-import { MaterialIcons } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 
-export default function RootLayout() {
+export default function TabsLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: "#fbc02d", // optional
-        tabBarInactiveTintColor: "gray", // optional
-        headerShown: false, // optional
+        headerTitleAlign: "center",
+        headerTitleStyle: {
+          fontSize: 20,
+          fontWeight: "bold",
+        },
+        headerStyle: { backgroundColor: "#f5f5f5" },
+        headerShadowVisible: false,
+        tabBarStyle: {
+          backgroundColor: "#f5f5f5",
+          borderTopWidth: 0,
+          elevation: 0,
+          shadowOpacity: 0,
+        },
+
+        tabBarActiveTintColor: "#6200ee",
+        tabBarInactiveTintColor: "#666666",
+        tabBarLabelStyle: {
+          fontSize: 14, // Larger text size
+          fontWeight: "600", // Semi-bold
+          marginBottom: 6, // Space between icon and text
+        },
       }}
     >
       <Tabs.Screen
         name="index"
         options={{
-          title: "hello",
+          title: "Today's Habits",
           tabBarIcon: ({ color, size }) => (
-            <MaterialIcons name="home" size={size + 8} color={color} />
+            <Feather name="calendar" size={size} color={color} />
           ),
         }}
       />
-
       <Tabs.Screen
-        name="login"
+        name="streaks"
         options={{
-          title: "login0",
+          title: "Streaks",
           tabBarIcon: ({ color, size }) => (
-            <MaterialIcons name="chat-bubble" size={size} color={color} />
+            <Feather name="activity" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="add-habit"
+        options={{
+          title: "Add Habit",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="plus-circle" size={size} color={color} />
           ),
         }}
       />
